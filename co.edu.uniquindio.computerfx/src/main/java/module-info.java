321dcb2795652f8125c1af0c9bc2fddfcea9120d
AndresCepedaC <143088681+AndresCepedaC@users.noreply.github.com>
2024-03-01 02:53:12 -0500

    create proyect

---
+++ co.edu.uniquindio.computerfx/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.computerfx.computerapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.computerfx.computerapp to javafx.fxml;
+    exports co.edu.uniquindio.computerfx.computerapp;
+}
